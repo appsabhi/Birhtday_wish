b35804e5ce0bfafbfcfcfc760c5babf9e80847f9
Abhinand P <abhinandappuabhinand@gmail.com>
2024-11-16 17:14:08 +0530

    updated with adding input file uploading image

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Birthday_wishing_Page from "./components/Birthday_wishing_Page";
 import {wish_context} from "./components/wish_context"
 
@@ -6,21 +6,42 @@ const App = () => {
 
 let [wish,setwish]= useState(false)
 let [name,setname] = useState(null)
+let [image,setimage] = useState(null)
+let reader = new FileReader
+
  let inputref = useRef()
 
+ let fileaccess=(e)=>{
+  const file = e.target.files[0]
+  reader.onload=(()=>{
+    setimage(reader.result)
+ 
+  })
+  reader.readAsDataURL(file)
 
-  
+ }
+
+useEffect(()=>{
+console.log(inputref.current)
+
+},[wish])  
   return (
-    <wish_context.Provider  value={{wish,name}}>
+    <wish_context.Provider  value={{wish,name,image}}>
     <div className="w-full h-full sm:w-full md:w-full lg:w-full xl:w-full  bg-gradient-to-tr  from-orange-100  to-green-300  relative  z-10">
       <div className="w-full h-1/2 sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2   rounded-xl absolute z-20 top-1/2 left-1/2 -translate-x-1/2  -translate-y-1/2  shadow-md shadow-black bg-orange-500">
         <p className="w-full h-1/5 text-center place-content-center text-xl text-blue-600 ">
           Welcome 
         </p>
-        <div className="w-full h-1/5 flex justify-center sm:w-full md:w-full lg:w-full xl:w-full ">
-          <input ref={inputref} onChange={()=>setname(inputref.current.value)} className="w-3/4 sm:w-3/4 md:w-3/4  lg:w-1/2 xl:w-1/2 rounded-lg bg-gradient-to-tr from-indigo-500 to-pink-400" type="text"  placeholder="Please enter your name"/>
+       
+        <div className="w-full h-1/4 flex flex-col gap-5 items-center sm:w-full md:w-full lg:w-full xl:w-full  ">
+         
+        <input onChange={(e)=>{fileaccess(e)}} className="w-3/4 h-1/2 sm:w-3/4 md:w-3/4  lg:w-1/2 xl:w-1/2  rounded-lg w- bg-gradient-to-tr from-indigo-500 to-pink-400" type="file" name="" id="" />
+         
+          <input ref={inputref} onChange={()=>setname(inputref.current.value)} className="w-3/4 h-1/2 sm:w-3/4 md:w-3/4  lg:w-1/2 xl:w-1/2 rounded-lg bg-gradient-to-tr from-indigo-500 to-pink-400" type="text"  placeholder="Please enter your name"/>
+
         </div>
-        <div className="w-full h-3/5 absolute z-30 sm:w-1/2 md:w-full lg:w-full xl:w-full flex justify-center items-center  md:flex md:justify-center ">
+
+        <div className="w-full h-3/5 absolute z-30 sm:w-full md:w-full lg:w-full xl:w-full flex justify-center items-center  md:flex md:justify-center  ">
           <button  onClick={()=>setwish(!wish)} className="w-1/2  sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 h-20 bg-green-500 text-center  rounded-lg cursor-pointer shadow-inner shadow-black   ">
             {" "}
             Tap to Reveal the Magic 🎉
--- src/components/Birthday_wishing_Page.jsx
+++ src/components/Birthday_wishing_Page.jsx
@@ -2,15 +2,14 @@ import { useContext, useEffect, useRef, useState } from "react";
 import { wish_context } from "./wish_context";
 import confetti from "canvas-confetti";
 import birthday_song from "../assets/Music/happy-birthday-song.mp3";
-import birthday_image from "../assets/images/akhila_image.jpg";
-import mute from "../assets/images/mute_icon.svg"
 
-
+import mute from "../assets/images/mute_icon.svg";
 
 const Birthday_wishing_Page = () => {
-  const { wish, name } = useContext(wish_context);
+  const { wish, name, image } = useContext(wish_context);
   const audioRef = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
+  //   const reader = new FileReader()
 
   // Play Audio
   const playAudio = () => {
@@ -38,9 +37,9 @@ const Birthday_wishing_Page = () => {
       decay: 0.9,
       origin: { x: 0.5, y: 0.5 },
       gravity: 0.7,
-      colors: ['#ff6347', '#ff0', '#ff69b4', '#00ff00', '#1e90ff', '#800080'],
-      shapes: ['circle', 'square', 'star', 'triangle'],
-      zIndex: 10000
+      colors: ["#ff6347", "#ff0", "#ff69b4", "#00ff00", "#1e90ff", "#800080"],
+      shapes: ["circle", "square", "star", "triangle"],
+      zIndex: 10000,
     });
   };
 
@@ -66,26 +65,33 @@ const Birthday_wishing_Page = () => {
           ? "w-full h-5/6 sm:w-full md:w-full lg:w-1/2 xl:w-1/2 bg-gradient-to-tr from-pink-500 to-yellow-400 animate-gradient bg-blast sm:text-xs md:text-sm lg:text-base xl:text-xl absolute z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col rounded-lg shadow-black shadow-lg "
           : "w-1/2 h-1/2 bg-green-900 text-white text-3xl absolute z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 hidden "
       }
-      
     >
       <div className="w-full h-3/5 sm:1/2 md:w-1/2 lg:w-full xl:w-full flex justify-center ">
         <img
-          src={birthday_image}
+          src={image}
           alt="Birthday Card"
           className="w-full h-full sm:w-full md:w-1/2 lg:w-1/2 xl:w-1/2 mb-4"
         />
       </div>
-      <p className="w-full h-2/5 text-center place-content-end absolute z-60 text-2xl top-3/4 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="w-full h-2/5 text-center place-content-end absolute z-60 text-2xl top-3/4 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <p className="text-pink-800">
           Wishing you all the happiness in the world! 🥳
         </p>
-        <strong className="text-yellow-400">🎉 Happy Birthday, {name}! 🎉</strong>
-      </p>
-      <audio ref={audioRef} src={birthday_song} loop aria-label="Birthday Song" />
-      
-      <button 
-        onClick={isPlaying ? stopAudio:playAudio} 
-        className="absolute top-4 right-4 p-2 bg-red-600 text-white rounded-full hover:bg-red-700 transition-all">
+        <strong className="text-yellow-400">
+          🎉 Happy Birthday, {name}! 🎉
+        </strong>
+      </div>
+      <audio
+        ref={audioRef}
+        src={birthday_song}
+        loop
+        aria-label="Birthday Song"
+      />
+
+      <button
+        onClick={isPlaying ? stopAudio : playAudio}
+        className="absolute top-4 right-4 p-2 bg-red-600 text-white rounded-full hover:bg-red-700 transition-all"
+      >
         <img src={mute} alt="" />
       </button>
     </div>
